0b9539d70ed73b6bdde9a0a20aba988da2e71b79
Harol Torres <harol003@gmail.com>
2024-08-14 16:34:36 -0500

    14082024

---
+++ AgendaTelefonica/src/agendatelefonica/AgendaTelefonica.java
@@ -0,0 +1,24 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package agendatelefonica;
+
+/**
+ *
+ * @author Software
+ */
+public class AgendaTelefonica {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        FormularioAgenda Form1 = new FormularioAgenda ();
+        Form1.setLocationRelativeTo(null);
+        Form1.setVisible(true);
+        
+    }
+    
+}
---
+++ Paradigma/src/paradigma/Paradigma.java
@@ -0,0 +1,24 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package paradigma;
+
+/**
+ *
+ * @author Software
+ */
+public class Paradigma {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        Formulario Form = new Formulario();
+        Form.setLocationRelativeTo(null);
+        Form.setVisible(true);
+        
+    }
+    
+}
